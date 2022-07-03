a4fbba0d85bddf9016b11c684177a99ea0cdc9d5
Javokhir <javokhir.bozorov@gmail.com>
2022-07-03 12:10:37 +0100

    some changes

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,9 +1,15 @@
 import React from "react";
 import Title from "../components/Title";
-
+import aboutMain from "../images/AboutMain.jpg"
 export default function About() {
   return <div>
-    About
-    <h1>Hello</h1>
+
+  <div className="page-body">
+
+  <Title>About Us</Title>
+{/* img here */}
+{/* <img src={aboutMain} alt="mainImage"> */}
+{/*  */}
+  </div>
     </div>;
 }
